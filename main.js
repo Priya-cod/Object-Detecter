8623d97be48e2da26f646f09b7e458de29608a78
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-06-10 15:41:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 img = "";
+status = "";
+objects = [];
 
-function setup() {
-    canvas = createCanvas(640, 420);
-    canvas.center();
-}
-
-function preload(){
-ing = loadImage('dogcat.jpg');
+function preload() {
+    img = loadImage('dog_cat.jpg');
 }
 
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
-    text("Dog", 45, 75);
-    stroke("FF0000");
-    Rect(30, 60, 450, 350 );
+
+        if(status != "")
+        {
+            for (i = 0; i < objects.lenght; i++)
+            {
+                document.getElementById("status").innerHTML = "Status : Object Detected";
+
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+        }
 }
