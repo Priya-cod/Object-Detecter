81d8dfdf28d162c78d6d355cef703c2d1eef79c1
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-06-12 13:30:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,11 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Object";
 }
@@ -16,7 +19,6 @@ function setup() {
 function modelLoaded() {
     console.log("Model Loaded!");
     status = true;
-    objectDetector.detect(img, gotResult);
 }
 
 function gotResult(error, results) {
@@ -24,20 +26,26 @@ function gotResult(error, results) {
         console.error(error);
     }
     console.log(results);
+    objects = results;
 }
 
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
 
         if(status != "")
         {
-            for (i = 0; i < objects.lenght; i++)
+            r = ramdom(255);
+            g = ramdom(255);
+            b = ramdom(255);
+            objectDetector.detect(video, gotResult);
+            for (i = 0; i < objects.length; i++)
             {
                 document.getElementById("status").innerHTML = "Status : Object Detected";
+                document.getElementById("number_of_object").innerHTML = "Number of objects detected are : "+ object.length;
 
                 percent = floor(objects[i].confidence * 100);
                 text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
-                stroke("#FF0000");
+                stroke(r,g,b)
                 rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
             }
         }
